5185ce4e6172290450b488c82eed1aea4de468dd
Maks7594 <72873506+Maks7594@users.noreply.github.com>
2024-07-28 00:07:21 +0200

    autoplayer

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -558,7 +558,7 @@ class PlayState extends MusicBeatState
 		updateScore(false);
 		uiGroup.add(scoreTxt);
 
-		botplayTxt = new FlxText(400, timeBar.y + 55, FlxG.width - 800, "BOTPLAY", 32);
+		botplayTxt = new FlxText(400, timeBar.y + 55, FlxG.width - 800, "AUTOPLAYER", 32);
 		botplayTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		botplayTxt.scrollFactor.set();
 		botplayTxt.borderSize = 1.25;
